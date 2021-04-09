ec1d30e68227ff41021395be5369504a5c6bda2d
daeYoungH <dy.hwangs@gmail.com>
2021-04-09 14:38:27 +0900

    Config redux

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const App = () => {
+  return(
+    <div>
+
+    </div>
+  )
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+import App from './App';
+import './index.css';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
